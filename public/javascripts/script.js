24da7ac255a3ab00dff815edf4a787ea8085de68
sohelhussain <sohelhussaing@gmail.com>
2024-05-11 23:32:50 +0530

    wip: feed page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,3 +10,18 @@ $('.go-sign').onclick = e => {
     $('.sing-form').style.display = 'flex';
     $('.login-form').style.display = 'none';
 }
+
+const navSlide = () => {
+    const lines = document.querySelector('.lines');
+    const nav = document.querySelector('.navLink');
+    
+    lines.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+        
+    //Line Animation
+    lines.classList.toggle('toggle');
+    });
+   
+}
+
+navSlide();
